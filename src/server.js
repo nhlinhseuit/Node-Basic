572b397948816610c97b12d6fee4b59bc6537b7e
nhlinhseuit <dev.hoanglinh@gmail.com>
2024-07-14 16:21:33 +0700

    middlware level app / level route

--- src/server.js
+++ src/server.js
@@ -4,11 +4,18 @@ import initWebRoute from './route/web'
 import initApiRoute from './route/api'
 
 // import connection from './configs/connectDB'
-
 require('dotenv').config()
+var morgan = require('morgan')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use((req, res, next) => {
+    console.log(req.header)
+    next()
+})
+
+app.use(morgan('combined'))
 
 // body parser
 app.use(express.urlencoded({ extended: true }))
@@ -22,6 +29,10 @@ initWebRoute(app)
 
 // init api route
 initApiRoute(app)
+
+app.use((req, res) => {
+    return res.render('404.ejs')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on http://localhost:${port}`)
